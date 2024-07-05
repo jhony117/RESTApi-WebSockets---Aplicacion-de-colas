ab4590c64714ebd95b1bb8444a5acf76cea47cf8
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-07-05 13:25:54 -0600

    conectrar servidores Express-WS

---
+++ src/presentation/services/wss.service.ts
@@ -0,0 +1,47 @@
+import { Server } from 'http';
+import  { WebSocket, WebSocketServer} from 'ws';
+
+interface Options {
+    server:Server;
+    path?:string;
+}
+
+
+
+export class WssService {
+    private  static _instance : WssService ;
+    private wss : WebSocketServer;
+
+
+
+    private constructor(options : Options){
+
+        const  {server, path = '/ws'} = options;
+
+
+        this.wss =  new WebSocketServer({server, path});
+
+    }
+
+    static get instance(): WssService{
+        if(!WssService._instance){
+            throw 'WeeService is not initialized'
+        }
+
+        return WssService._instance;
+    }
+
+    static initWss(options:Options){
+   WssService._instance =  new WssService(options);
+    }
+
+    public start() {
+
+        this.wss.on('connectoin', (ws :WebSocket) => {
+
+            console.log('Cloietn connectes')
+            console.log('close', () => console.log('clienf'));
+
+        });
+    } 
+}
