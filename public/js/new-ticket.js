34d4c9ea2c4069a83fc7cd6aca1e1258546feb06
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-07-05 15:06:54 -0600

    pantalla nuevo ticket

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,17 +1,25 @@
 
 
+const currentTicketLbl = document.querySelector('span');
+const createTicketBtn =  document.querySelector('button');
 
 
+ getLastTicket  = async() => {
+  const lastTicket = await fetch('/api/ticket/last').then(resp =>resp.json());
+  currentTicketLbl.innerText = lastTicket;
+}
 
+async function createTicket() {
+    const newTicket = await fetch('/api/ticket', {
+        method: 'POST'
+    }).then(resp => resp.json());
 
+    currentTicketLbl.innerText = newTicket.number;
+}
 
+createTicketBtn.addEventListener('click', createTicket);
 
-
-
-
-
-
-
+getLastTicket();
 
 
 
